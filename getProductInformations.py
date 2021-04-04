8a52e242a4532e18023dba7799407c9a92bf700c
Juan Lessa <juanvlessa@ua.pt>
2021-04-04 15:26:54 +0100

    complete get product informations

---
+++ getProductInformations.py
@@ -0,0 +1,28 @@
+import urllib.request
+import requests
+from datetime import datetime
+
+
+def getProductInformations(productId: str):
+    baseURL = 'https://cosmetics2.vtexcommercestable.com.br/api/catalog_system/pub/products/variations/'
+
+    # busca informações do produto
+    response = requests.get(baseURL + productId)
+    data = response.json()
+
+    # nome do produto
+    name = data['name']
+    # preço do produto
+    price = data['skus'][0]["bestPriceFormated"]
+    # url da imagem do produto
+    url = data['skus'][0]['image']
+
+    fileName = 'product:' + datetime.now().strftime("%m-%d-%Y-%H-%M-%S%f") + '.png'
+    # busca e salva imagem do produto no arquivo"fileName"
+    urllib.request.urlretrieve(url, fileName)
+
+    return {"productId": productId,
+            "url": url,
+            "imageFileName": fileName,
+            "price": price,
+            'name': name}
